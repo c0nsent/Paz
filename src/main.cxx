0b9089fa595e1c53e56856bdef0be017e9238fbc
consent <c0nsent@proton.me>
2025-09-25 19:17:47 +0300

    Set up basic Qt6 project structure and added initial main window implementation

---
+++ src/main.cxx
@@ -0,0 +1,11 @@
+#include <QtWidgets>
+
+int main(int argc, char *argv[])
+{
+	QApplication app(argc, argv);
+	QWidget window{};
+	window.resize(800, 600);
+	window.show();
+	window.setWindowTitle( "Paz" );
+	return QApplication::exec();
+}
