dbb4348b982d0ea2b3f2f957770e1f11d2a5bd0b
consent <c0nsent@proton.me>
2025-11-15 20:43:48 +0300

    Fix typo in class name and enhance `PomodoroTimerBackend` with pomodoros cycle support

--- src/backend/pomodoro-timer-backend.cpp
+++ src/backend/pomodoro-timer-backend.cpp
@@ -6,117 +6,137 @@
 
 #include "pomodoro-timer-backend.hpp"
 
-
-paz::backend::PomodoroTImerBackend::PomodoroTImerBackend( const quint16 workTime, const quint16 shortBreakTime, const quint16 longBreakTime, QObject *parent )
-	: QObject{parent}, m_workTime{workTime}, m_shortBreakTime{shortBreakTime}, m_longBreakTime{longBreakTime}, m_currentTime{0}, m_state{State::paused}
+namespace paz::backend
 {
-	m_timer.setInterval(1000);
-
-	QObject::connect(&m_timer, &QTimer::timeout, this, &PomodoroTImerBackend::timerTicked);
-
-}
-
-
-quint16 paz::backend::PomodoroTImerBackend::getWorkTime() const
-{
-}
-
-
-quint16 paz::backend::PomodoroTImerBackend::getShortBreakTime() const
-{
-}
-
-
-quint16 paz::backend::PomodoroTImerBackend::getLongBreakTime() const
-{
-}
-
-
-paz::backend::PomodoroTImerBackend::State paz::backend::PomodoroTImerBackend::getState() const
-{
-}
-
-
-quint16 paz::backend::PomodoroTImerBackend::getCurrentTime() const
-{
-}
-
-
-void paz::backend::PomodoroTImerBackend::setWorkTime( quint16 workTime )
-{
-}
-
-
-void paz::backend::PomodoroTImerBackend::setShortBreakTime( quint16 shortBreakTime )
-{
-}
-
-
-void paz::backend::PomodoroTImerBackend::setLongBreakTime( quint16 longBreakTime )
-{
-}
-
-
-void paz::backend::PomodoroTImerBackend::setState( State state )
-{
-}
-
-
-void paz::backend::PomodoroTImerBackend::setCurrentTime( quint16 currentTime )
-{
-}
-
-
-void paz::backend::PomodoroTImerBackend::start()
-{
-}
-
-
-void paz::backend::PomodoroTImerBackend::stop()
-{
-}
-
-
-void paz::backend::PomodoroTImerBackend::resume()
-{
-}
-
-
-void paz::backend::PomodoroTImerBackend::pause()
-{
-}
-
-
-void paz::backend::PomodoroTImerBackend::reset()
-{
-}
-
-
-void paz::backend::PomodoroTImerBackend::timerTicked()
-{
-	m_currentTime++;
-
-	switch (m_state)
+	PomodoroTimerBackend::PomodoroTimerBackend( const quint16 workTime, const quint16 shortBreakTime,
+		const quint16 longBreakTime, const quint16 pomodorosInCycle, QObject *parent )
+	: QObject{parent}, m_workTime{workTime}, m_shortBreakTime{shortBreakTime}
+	, m_longBreakTime{longBreakTime}, m_pomodorosInCycle{pomodorosInCycle}
 	{
-		case State::work:
-			if (m_currentTime == m_workTime)
-			{
-				m_currentTime = 0;
-				m_state = State::shortBreak;
-			}
-			break;
-		case State::shortBreak:
-			if (m_currentTime == m_shortBreakTime)
-			{
-				m_currentTime = 0;
-				m_state
-			}
+		m_timer.setInterval(1000);
 
+		QObject::connect(&m_timer, &QTimer::timeout, this, &PomodoroTimerBackend::timerTicked);
 	}
 
-}
+
+	quint16 PomodoroTimerBackend::getWorkTime() const { return m_workTime; }
 
 
-void paz::backend::PomodoroTImerBackend::timerFinished() const
-{
+	quint16 PomodoroTimerBackend::getShortBreakTime() const { return m_shortBreakTime; }
+
+
+	quint16 PomodoroTimerBackend::getLongBreakTime() const { return m_longBreakTime; }
+
+
+	quint16 PomodoroTimerBackend::getPomodorosInCycle() const { return m_pomodorosInCycle; }
+
+
+	quint16 PomodoroTimerBackend::getCurrentCyclePomodoroCount() const { return m_currentCyclePomodoroCount; }
+
+
+	void PomodoroTimerBackend::setPomodorosInCycle( const quint16 pomodorosInCycle )
+	{
+		this->m_pomodorosInCycle = pomodorosInCycle;
+	}
+
+
+	void PomodoroTimerBackend::setCurrentCyclePomodoroCount(const quint16 currentCyclePomodoroCount )
+	{
+		this->m_currentCyclePomodoroCount = currentCyclePomodoroCount;
+	}
+
+
+	PomodoroTimerBackend::State PomodoroTimerBackend::getState() const { return m_state; }
+
+
+	quint16 PomodoroTimerBackend::getCurrentTime() const
+	{
+	}
+
+
+	void PomodoroTimerBackend::setWorkTime( quint16 workTime )
+	{
+	}
+
+
+	void PomodoroTimerBackend::setShortBreakTime( quint16 shortBreakTime )
+	{
+	}
+
+
+	void PomodoroTimerBackend::setLongBreakTime( quint16 longBreakTime )
+	{
+	}
+
+
+	void PomodoroTimerBackend::setState( State state )
+	{
+	}
+
+
+	void PomodoroTimerBackend::setCurrentTime( quint16 currentTime )
+	{
+	}
+
+
+	void PomodoroTimerBackend::start()
+	{
+	}
+
+
+	void PomodoroTimerBackend::stop()
+	{
+	}
+
+
+	void PomodoroTimerBackend::resume()
+	{
+	}
+
+
+	void PomodoroTimerBackend::pause()
+	{
+	}
+
+
+	void PomodoroTimerBackend::reset()
+	{
+	}
+
+
+	void PomodoroTimerBackend::timerTicked()
+	{
+		if (m_state == State::paused) return;
+
+		m_currentTime++;
+
+		if (m_state == State::work and m_currentTime >= m_workTime)
+		{
+			if (m_currentCyclePomodoroCount < m_pomodorosInCycle)
+			{
+				m_currentCyclePomodoroCount++;
+				m_state = State::shortBreak;
+			}
+			else
+			{
+				m_currentCyclePomodoroCount = 0;
+				m_state = State::longBreak;
+			}
+
+			emit timerFinished();
+		}
+		else if (m_currentTime >= m_shortBreakTime or m_currentTime >= m_longBreakTime)
+		{
+			m_state = State::work;
+
+			emit timerFinished();
+		}
+	}
+
+
+	void PomodoroTimerBackend::timerFinished()
+	{
+		m_currentTime = 0;
+	}
+
 }
--- src/backend/pomodoro-timer-backend.hpp
+++ src/backend/pomodoro-timer-backend.hpp
@@ -15,7 +15,7 @@
 
 namespace paz::backend
 {
-	class PomodoroTImerBackend : public QObject
+	class PomodoroTimerBackend : public QObject
 	{
 		Q_OBJECT
 
@@ -23,19 +23,23 @@ namespace paz::backend
 
 		enum class State : quint8 { work, shortBreak, longBreak, paused };
 
-		PomodoroTImerBackend(quint16 workTime, quint16 shortBreakTime, quint16 longBreakTime , QObject* parent = nullptr);
+		PomodoroTimerBackend(quint16 workTime, quint16 shortBreakTime, quint16 longBreakTime, quint16 pomodorosInCycle, QObject* parent = nullptr);
 
-		quint16 getWorkTime() const;
-		quint16 getShortBreakTime() const;
-		quint16 getLongBreakTime() const;
-		State getState() const;
-		quint16 getCurrentTime() const;
+		[[nodiscard]] quint16 getWorkTime() const;
+		[[nodiscard]] quint16 getShortBreakTime() const;
+		[[nodiscard]] quint16 getLongBreakTime() const;
+		[[nodiscard]] quint16 getPomodorosInCycle() const;
+		[[nodiscard]] quint16 getCurrentTime() const;
+		[[nodiscard]] quint16 getCurrentCyclePomodoroCount() const;
+		[[nodiscard]] State getState() const;
 
 		void setWorkTime(quint16 workTime);
 		void setShortBreakTime(quint16 shortBreakTime);
 		void setLongBreakTime(quint16 longBreakTime);
-		void setState(State state);
+		void setPomodorosInCycle(quint16 pomodorosInCycle);
 		void setCurrentTime(quint16 currentTime);
+		void setCurrentCyclePomodoroCount(quint16 currentCyclePomodoroCount);
+		void setState(State state);
 
 	public slots:
 
@@ -48,17 +52,19 @@ namespace paz::backend
 	signals:
 
 		void timerTicked();
-		void timerFinished() const;
+		void timerFinished();
 
 	private:
 
-		QTimer m_timer;
+		QTimer m_timer{};
 
 		quint16 m_workTime;
 		quint16 m_shortBreakTime;
 		quint16 m_longBreakTime;
-		quint16 m_currentTime;
+		quint16 m_pomodorosInCycle;
 
-		State m_state;
+		quint16 m_currentTime{0};
+		quint16 m_currentCyclePomodoroCount{0};
+		State m_state{State::paused};
 	};
 }
