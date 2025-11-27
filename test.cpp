22600b0a4cf4bd280180908502f991b6d2fd729f
consent <c0nsent@proton.me>
2025-11-27 18:45:39 +0300

    feat(build): restructure CMake configuration and add Qt6 dependencies

---
+++ test.cpp
@@ -0,0 +1,17 @@
+/**
+ * @date 11/27/25
+ * 
+ * @author amitayus_
+ */
+
+#include <algorithm>
+#include <print>
+#include <iostream>
+#include <QDebug>
+
+int main()
+{
+ constexpr qint16 var{22};
+ constexpr qint16 min{12};
+ constexpr qint16 max{22};
+}
