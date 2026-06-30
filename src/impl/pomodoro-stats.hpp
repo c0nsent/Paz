c2dc270dcb6885dae62fb01303cd5988af895d94
amitayus_ <c0nsent@proton.me>
2026-06-30 23:53:54 +0300

    refactor(stats): extend `PomodoroStats` functionality with data lookup, modification, and synchronization methods

--- src/impl/pomodoro-stats.hpp
+++ src/impl/pomodoro-stats.hpp
@@ -6,8 +6,12 @@
 #include <QHash>
 #include <QList>
 #include <QDate>
+#include <QObject>
+#include <QVariant>
 #include <QTime>
 #include <QVarLengthArray>
+
+#include <optional>
 
 
 namespace impl
@@ -23,22 +27,29 @@ namespace impl
             QDate date;
             u16 pomodoros;
             QTime totalTime;
+
+            bool operator==(const DataEntry &rhs) const;
+            bool operator==(QDate otherDate) const;
         };
 
         explicit PomodoroStats(QObject *parent = nullptr);
 
-        [[nodiscard]] auto at(QDate date) -> DataEntry &;
-        [[nodiscard]] auto checkout(QDate date) const -> DataEntry;
+    [[nodiscard]] auto at(QDate date) -> std::optional<DataEntry &>;
+        [[nodiscard]] auto checkout(QDate date) const -> std::optional<DataEntry>;
         [[nodiscard]] bool contains(QDate date) const;
         [[nodiscard]] auto range(QDate begin, QDate end) const -> QVarLengthArray<DataEntry>;
         [[nodiscard]] auto size() const -> size_t;
 
-
     public slots:
 
-        void addEntry(QDate date, u16 pomodoros, QTime totalTime);
+        //void addEntry(QDate date, u16 pomodoros, QTime totalTime);
+        void addPomodoro(QTime pomodoroDuration, QDate date=QDate::currentDate());
+        void sync();
 
+    signals:
 
+        void entryAdded(QDate date);
+        void entryChanged(QDate date);
 
     private:
 
